2f3f18271f839c351246d81bdc10c603e7bf2db8
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-30 13:49:32 +0300

    silva

--- plugins/owner-restart.js
+++ plugins/owner-restart.js
@@ -2,7 +2,7 @@ import { spawn } from 'child_process'
 let handler = async (m, { conn, isROwner, text }) => {
   if (!process.send) throw 'Dont: node main.js\nDo: node index.js'
   if (conn.user.jid == conn.user.jid) {
-    await m.reply('🔄 Restarting Bot...\n Wait a moment')
+    await m.reply('🔄 sᵢₗᵥₐ bₒt Restarting Bot...\nyou are too horny thats why\nyou are restarting me\n Wait a moment')
     process.send('reset')
   } else throw 'eh'
 }
