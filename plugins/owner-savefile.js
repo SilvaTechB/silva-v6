02c87b03d2ccf69b966e2142064ed1c8e6901e05
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-30 13:50:58 +0300

    silva

--- plugins/owner-savefile.js
+++ plugins/owner-savefile.js
@@ -1,6 +1,6 @@
 import fs from 'fs' 
  let handler = async (m, { text, usedPrefix, command }) => { 
-     if (!text) throw `uhm.. where is the text?\n\nUsage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} plugins/xei-sensei.js` 
+     if (!text) throw `uhm.. where is the text?\n\nUsage:\n${usedPrefix + command} <text>\n\nExample:\n${usedPrefix + command} plugins/silva.js` 
      if (!m.quoted.text) throw `reply to the message!` 
      let path = `${text}` 
      await fs.writeFileSync(path, m.quoted.text) 
