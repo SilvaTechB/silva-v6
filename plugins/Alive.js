77086fa562f270c33d2f01ec192a9838fc65e8a4
Silva Tech <163982940+SilvaTechB@users.noreply.github.com>
2024-07-30 14:17:47 +0300

    silva

--- plugins/Alive.js
+++ plugins/Alive.js
@@ -1,8 +1,8 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
   // Sound
   let name = m.pushName || conn.getName(m.sender)
-  var vn = 'https://cdn.jsdelivr.net/gh/Lazack28/Lazack-md@main/media/Alive.mp3'
-  let url = 'https://github.com/Lazack28/Lazack-md'
+  var vn = 'https://cdn.jsdelivr.net/gh/SilvaTechB/silva-v6@main/media/Alive.mp3'
+  let url = 'https://github.com/SilvaTechB/silva-v6'
   let murl = 'https://youtu.be/3j_EIP--2t8?si=4TFWV0On6Bl1wr-e'
   let img = 'https://i.imgur.com/QMyKIPq.jpeg'
   let con = {
@@ -30,10 +30,10 @@ let handler = async (m, { conn, text, usedPrefix, command }) => {
     contextInfo: {
       mentionedJid: [m.sender],
       externalAdReply: {
-        title: 'LAZACK MD ALIVE',
-        body: 'Lazack md',
+        title: '▁ ▂ ▃ ▅ ▆ ▇ ▌SILVA BOT▐ ▇ ▆ ▅ ▃ ▂ ▁ ALIVE',
+        body: '▞▞▞▞ 🆂🅸🅻🆅🅰 🅱🅾🆃 ▞▞▞▞',
         thumbnailUrl: img,
-        sourceUrl: 'https://chat.whatsapp.com/IIpL6gf6dcq4ial8gaJLE9',
+        sourceUrl: 'https://chat.whatsapp.com/Lr80ac3MKKIKGew8mFES2a',
         mediaType: 1,
         renderLargerThumbnail: true,
       },
